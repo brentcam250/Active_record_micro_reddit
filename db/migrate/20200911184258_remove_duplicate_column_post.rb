1e3ff6732e5e24ede08cebad5c5adc3270356bce
Brent Cameron <bjcameron@gmail.com>
2020-09-11 13:05:16 -0600

    fixed bugs in post association with user

---
+++ db/migrate/20200911184258_remove_duplicate_column_post.rb
@@ -0,0 +1,5 @@
+class RemoveDuplicateColumnPost < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :posts, :user_id
+  end
+end
---
+++ db/migrate/20200911184556_remove_idid.rb
@@ -0,0 +1,5 @@
+class RemoveIdid < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :posts, :user_id_id
+  end
+end
